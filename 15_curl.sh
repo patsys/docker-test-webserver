bd28a60a175c20b6da08bb09d645dc2b144ae416
th <th@centos.patsys.local>
2016-12-03 05:02:37 +0100

    first commit

---
+++ 15_curl.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+if [ -z "$INTERVAL" ]; then
+    INTERVAL=5
+fi  
+if [ -z "$TRY" ]; then
+    TRY=30
+fi 
+
+number=0
+while [ "$number" -lt "$TRY" ]
+do
+  number=`expr $number + 1 `
+  STATUS=$(curl --connect-timeout $INTERVAL  -s -o /dev/null -w '%{http_code}' "$HOST")
+  if [ $STATUS -eq 200 ]; then
+     if [ -z "$WORD" ]; then
+       exit 0; 
+     else
+       BODY=`curl -L $HOST` 
+       if echo "$BODY" | grep -q "$WORD" 
+       then
+         exit 0
+       else
+         echo "WORD not exist"
+         exit 2
+       fi
+     fi
+  elif [ $STATUS -ne 0 ]; then 
+    echo "Response $STATUS"
+    exit 2
+  fi
+done
+echo "SERVER not start"
+exit 1
---
+++ README.md
@@ -0,0 +1,9 @@
+# DOcker test-webserver
+
+Alpine based curl with script to test a Webserver is running then return 0
+
+Parameter:
+HOST: Site to test
+INTERVAL: Timeinterval in seconds before start a new connect
+TRY: Number of new connection
+WORD: test if site exist a word
